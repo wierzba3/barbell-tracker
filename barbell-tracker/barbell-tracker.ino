c4860be7097b3650fbd7ec9abbe8c2fe74338ad8
wierzba3 <jamesmw129@gmail.com>
2016-03-27 15:43:55 -0600

    - apply a fix for the special case of the lifter re-racking the weight, and extraneous velocity values being read

--- barbell-tracker/barbell-tracker.ino
+++ barbell-tracker/barbell-tracker.ino
@@ -11,14 +11,9 @@ using namespace std;
 
 /**
  * BUGS:
- * - figure out why some reps say NAN and some are empty. A guess is some error, possibly dividing by zero?
- * 
  * 
  * TODO
- * - change time from millis to micros
- * - add a condition for the completion of the rep
- *     before it was if(went down minimum distance), now lets do: if(went down minimum distance OR minimum time has elasped without going up)
- *     this is for the last rep, where the lifter will rack the weight, and the rep will not be calculated because there is no more downward movement
+ * - figure out a way to remove the extra time steps from 
  * - figure out why encoder steps are being missed. 
  *     Could be the cheap encoder, or possibly too much CPU time spent on code, before the next read.
  *     Try using interupts instead of polling. See example: http://playground.arduino.cc/Main/RotaryEncoders#Example3
@@ -33,13 +28,13 @@ const int MAX_VALUE = 32767; //max integer value
 const int MIN_VALUE = -32768; //min integer value
 int repCounter = 1;
 const int TICKS_PER_CM = 4.098; //the amount of turns of the rotary encoder per cm of the string pulled
-int minDistance = 5 * TICKS_PER_CM; //the minimum amount of cm the string must move in the opposite direction to be determined as switched direction
+
 
 int minValue = MAX_VALUE; //Variable that keeps track of the most recent minimum counter value, and the time at which it were hit.
 int maxValue = MIN_VALUE; //Variable that keeps track of the most recent minimum counter value, and the time at which it were hit.
 int counter = 0; //counter is incremented as the string is pulled, and decremented as the string retracts
 int lastCounter = 0; //the previous counter value at which a time was recorded
-int counterInterval = 5; //the value at which each time sample is taken.
+
 
 vector<long> timeSteps; //A list of time values taken every <counterInterval> counter steps
 vector<double> avgVelocityValues;
@@ -49,7 +44,15 @@ bool stringRetracting = true; //true if the string is retracting (the lifter is 
 bool stringPulling = false; //true if the string is being pulled (the lifter is in the eccentric phase of the exercise)
 
 int8_t encoderStep; //holds the temporary value read from the quadrature encoder, -1, 0, or 1 which signify clockwise, invalid, counter-clockwise respectively
- 
+
+//settings: (create a way to modify these variables via user-input in the future)
+long repEndWaitTime = 5 * 1000000; //the amount of time to wait at the top of the rep with no activity to count it as a rep complete (in micros)
+int minDistance = 5 * TICKS_PER_CM; //the minimum amount of cm the string must move in the opposite direction to be determined as switched direction
+int counterInterval = 5; //the value at which each time sample is taken. 1cm ~= 4.098 counts
+//A distance from the end (in ticks) of the range of motion. All velocity values are scanned...
+int reRackDistance = 120; //...if a velocity value is found that is less than <minVelocity>, all subsequent values are truncated
+double minVelocity = 0.1;
+
 void setup()
 {
   /* Setup encoder pins as inputs */
@@ -60,6 +63,16 @@ void setup()
   Serial.begin (115200);
   Serial.println("Start");
   Serial.println("");
+}
+
+/* returns change in encoder state (-1,0,1) */
+int8_t read_encoder()
+{
+  static int8_t enc_states[] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
+  static uint8_t old_AB = 0;
+  old_AB <<= 2;                   //remember previous state
+  old_AB |= ( ENC_PORT & 0x03 );  //add current state
+  return ( enc_states[( old_AB & 0x0f )]);
 }
  
 void loop()
@@ -104,11 +117,8 @@ void loop()
       //if the distance moved upwards is greater than the minimum required, we conclude the exercise is now in the upwards (eccentric) phase
       if(diff > minDistance)
       {
-        stringRetracting = false;
-        stringPulling = true;
-        //RESET counter because inaccuraces with encoder readings may cause mis-steps, and possibly negative counter values.
-        counter = minValue = lastCounter = 0;
-        maxValue = MIN_VALUE;
+        switchDirection(true); //switch string state to pulling
+        return;
       }
       
     }
@@ -121,33 +131,66 @@ void loop()
       //if the distance moved downwards is greater than the minimum required, we conclude that the exercise is now in the downards (concentric) phase
       if(diff > minDistance)
       {
-        calculateVelocity();
-        stringRetracting = true;
-        stringPulling = false;
-        minValue = MAX_VALUE;
-        maxValue = MIN_VALUE;
+        switchDirection(false); //switch string state to retracting
+        return;
+      }
+    }
+
+    if(stringPulling)
+    {
+      //special case, check if the time since the last sample is greater than the minimum (currently 5 seconds)
+      if(timeSteps.size() > 0 && (micros() - timeSteps[timeSteps.size()-1]) > repEndWaitTime)
+      {
+        Serial.println("TIMEOUT");
+        switchDirection(false); //switch string state to retracting
+        return;
       }
     }
     
     
+    
   }
 }
- 
-/* returns change in encoder state (-1,0,1) */
-int8_t read_encoder()
+
+
+void switchDirection(bool pulling)
 {
-  static int8_t enc_states[] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
-  static uint8_t old_AB = 0;
-  /**/
-  old_AB <<= 2;                   //remember previous state
-  old_AB |= ( ENC_PORT & 0x03 );  //add current state
-  return ( enc_states[( old_AB & 0x0f )]);
+  if(pulling)
+  {
+    stringRetracting = false;
+    stringPulling = true;
+    //RESET counter because inaccuraces with encoder readings may cause mis-steps, and possibly negative counter values.
+    counter = minValue = lastCounter = 0;
+    maxValue = MIN_VALUE;
+  }
+  else
+  {
+    calculateVelocity();
+    stringRetracting = true;
+    stringPulling = false;
+    minValue = MAX_VALUE;
+    maxValue = MIN_VALUE;
+  }
 }
 
 void calculateVelocity()
 {
+  //TODO
+  //scan the last <repEndDistance> distance for velocity values less than <minDistance> and truncate the list from that point
+  //this is to remove the time samples between the rep completion and re-rack
+
+  //last index of time steps vector
+  int lastTimeIdx = timeSteps.size() - 1; 
+  
+  //the index at which we start checking for small velocity values to truncate at the end of the rep
+  int rerackIndex = lastTimeIdx - (reRackDistance / counterInterval); 
+  
   //remove the last time step, as the lifter will probably stop for breaths at the top, and it may read a new time step much later
+  //samples are taken every ~1cm, so let's remove 2 smaples to be sure
   timeSteps.pop_back();
+  timeSteps.pop_back();
+  
+
 
   Serial.println("");
   Serial.println("");
@@ -170,8 +213,13 @@ void calculateVelocity()
       if(dt == 0) continue;
       double stepsPerSec = counterInterval * 1000000.0 / dt;
       double metersPerSec = (stepsPerSec / TICKS_PER_CM) / 100.0;
+
+      //if we are at last portion of the lift, and we see a very small velocity, truncate the rest of the values (we assume the lifter finished and re-racked the weight)
+      if(i >= rerackIndex && metersPerSec < minVelocity) break;
+      
       prev = timeSteps[i];
       total += metersPerSec;
+      
       if(metersPerSec > maxVelocity) maxVelocity = metersPerSec;
       cnt++;
 
